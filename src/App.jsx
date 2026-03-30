05a6318b0fe0ae01dbf32f908c4378c88c2f57b9
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-03-30 23:29:51 +0600

    Project setup complete

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h2 className='text-red-500 font-bold text-3xl'>Assignment 6</h2>
+      
+    </div>
+  )
+}
+
+export default App
